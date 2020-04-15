5717dc45cdceff68c65d097c75ebc3c532268c97
unknown <cambuf@gmail.com>
2020-04-15 17:31:00 -0600

    hooks-for-singledrill-not-working

--- server/controllers/drillController.js
+++ server/controllers/drillController.js
@@ -11,7 +11,9 @@ module.exports = {
     getSingleDrill: (req, res) => {
         const db = req.app.get('db');
         const {drill_id} = req.params;
-        const {name, distance, shots, setup, partime, score, actions} =req
+        const {name, distance, shots, setup, partime, score, actions} = req.body
+
+        console.log(req.body)
 
         db.drills.get_single_drill(drill_id, name, distance, shots, setup, partime, score, actions)
         .then((results) => res.status(200).send(results))
--- src/components/SingleDrill.js
+++ src/components/SingleDrill.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, {useEffect, useSingle, useState} from 'react'
 import axios from 'axios'
 import useAxios from '../hooks/useAxios'
 
 
 
-const SingleDrill = ({props}) => {
-    const [drills,{getDrills}] = useAxios('singledrills/:drill_id')
+const SingleDrill = ({history, props}) => {
+    const [drills] = useSingle('/api/singledrill/:drill_id')
+    console.log(drills)
     return(
 
+
         <div>single drill
-            {drills.map(({name, distance, shots, setup, partime, actions}) =>(
-                <div>{drills.name}</div>
-                // <div>{distance}</div>
-                // <div>{shots}</div>
-                // <div>{setup}</div>
-                // <div>{partime}</div>
-                // <div>{actions}</div>
+            {
+                drills.map(drill => {
+                    return (
+                        <div>{drill.name}</div>
+                        // <div>{drills.drill_id}</div>
+                        // <div>{distance}</div>
+                        // <div>{shots}</div>
+                        // <div>{setup}</div>
+                        // <div>{partime}</div>
+                        // <div>{actions}</div>
                 
-            ))}
+                )})}
         </div>
         )
 }
