cc98659c71b07f6299156d09c3aa5d5683b67296
unknown <cambuf@gmail.com>
2020-04-15 11:35:04 -0600

    routes-running-off-navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import axios from 'axios'
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+import {getUser} from '../Duxx/reducer'
 
 
-const Nav = () => {
+const Nav = ({history}) => {
     return(
 
-        <div>nav</div>
+        <div>
+            DRILL LOG
+            <button onClick = {() => history.push('/')} >home</button>
+            <button onClick = {() => history.push('drills')}>drill archive</button>
+            <button onClick = {() => history.push('mytraining')}>my training</button>
+            
+        </div>
         )
 }
 
-export default Nav
+export default (withRouter(Nav))
